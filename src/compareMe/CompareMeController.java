9d1be971e9009e32ed742e2d62739fcb400cf670
dengitzjg <dengitzjg@gmail.com>
2016-12-23 23:44:12 -0500

    Added second directory selector

--- src/compareMe/CompareMeController.java
+++ src/compareMe/CompareMeController.java
@@ -1,21 +1,29 @@
 package compareMe;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.DirectoryChooser;
 
 import java.io.File;
 
 public class CompareMeController {
+    public Button selectDir1;
+    public Button selectDir2;
     public Label directory1;
+    public Label directory2;
 
     public void selectDirectory1(ActionEvent actionEvent) {
         directory1.setText(this.selectDirectory());
     }
 
+    public void selectDirectory2(ActionEvent actionEvent) {
+        directory2.setText(this.selectDirectory());
+    }
+
     private String selectDirectory() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
-        File selected = directoryChooser.showDialog(null);
-        return selected.getAbsolutePath();
+        File selected = directoryChooser.showDialog(selectDir1.getScene().getWindow());
+        return (selected != null) ? selected.getAbsolutePath() : null;
     }
 }
