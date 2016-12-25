2f9e8de8f528708905e04b00ab8b99f0ba2c0369
dengitzjg <dengitzjg@gmail.com>
2016-12-24 19:06:05 -0500

    Changed directory Labels to TextFields

--- src/compareMe/CompareMeController.java
+++ src/compareMe/CompareMeController.java
@@ -2,19 +2,16 @@ package compareMe;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 
 import java.io.File;
 
 public class CompareMeController {
-    /*
-    TODO: Change Label to an uneditable textbox
-     */
     public Button selectDir1;
     public Button selectDir2;
-    public Label directory1;
-    public Label directory2;
+    public TextField directory1;
+    public TextField directory2;
 
     public void selectDirectory1(ActionEvent actionEvent) {
         directory1.setText(this.selectDirectory());
