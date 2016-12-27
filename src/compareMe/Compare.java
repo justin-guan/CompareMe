c86f201405fa0bd5713f51dc8900e47cb04f3333
dengitzjg <dengitzjg@gmail.com>
2016-12-26 23:43:46 -0500

    Added preliminary logic for comparisons

--- src/compareMe/Compare.java
+++ src/compareMe/Compare.java
@@ -1,7 +1,29 @@
 package compareMe;
 
-/**
- * Created by JG on 12/25/2016.
- */
+import java.io.File;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Compare {
+    public static List<File> compare(String dir1, String dir2, String algorithm) throws NoSuchAlgorithmException {
+        HashMap<String, String> files = new HashMap<String, String>();
+        //ArrayList<> collisions = new ArrayList<Pair<String, String>>();
+        File directory = new File(dir1);
+        File[] directoryList = directory.listFiles();
+        if (directoryList != null) {
+            for (File f : directoryList) {
+                String hash = HashGenerator.generateHash(algorithm, f.getAbsolutePath());
+                if (files.containsKey(hash)) { // Collision
+
+
+                } else { // No Collision
+
+                }
+            }
+        } else {
+            return;
+        }
+    }
 }
