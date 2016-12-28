f6a342f18baa038481264b7fee0663a667670850
dengitzjg <dengitzjg@gmail.com>
2016-12-27 21:15:03 -0500

    Changed comparison to find all duplicates in a single directory

--- src/compareMe/Compare.java
+++ src/compareMe/Compare.java
@@ -2,28 +2,35 @@ package compareMe;
 
 import java.io.File;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Compare {
-    public static List<File> compare(String dir1, String dir2, String algorithm) throws NoSuchAlgorithmException {
-        HashMap<String, String> files = new HashMap<String, String>();
-        //ArrayList<> collisions = new ArrayList<Pair<String, String>>();
-        File directory = new File(dir1);
+    public static List<File> compare(String dir, String algorithm) throws NoSuchAlgorithmException {
+        HashMap<String, ArrayList<String>> files = new HashMap<String, ArrayList<String>>();
+        File directory = new File(dir);
         File[] directoryList = directory.listFiles();
         if (directoryList != null) {
             for (File f : directoryList) {
-                String hash = HashGenerator.generateHash(algorithm, f.getAbsolutePath());
-                if (files.containsKey(hash)) { // Collision
-
-
-                } else { // No Collision
-
+                if (f.isFile()) {
+                    String hash = HashGenerator.generateHash(algorithm, f.getAbsolutePath());
+                    if (!files.containsKey(hash)) {
+                        ArrayList<String> file = new ArrayList<String>();
+                        file.add(f.getAbsolutePath());
+                        files.put(hash, file);
+                    } else { // Collision
+                        files.get(hash).add(f.getAbsolutePath());
+                    }
                 }
             }
-        } else {
-            return;
+        } else { // Not a directory or no files
+            return null;
         }
+
+        for (ArrayList<String> a : files.values()) {
+            if (a.size() > 1) {
+                System.out.println(a.toString());
+            }
+        }
+        return null;
     }
 }
