d0a8532ac20a5651fd2b6f6a78ceb86465d08b13
dengitzjg <dengitzjg@gmail.com>
2016-12-30 01:42:32 -0500

    Added preliminary deleter window

---
+++ src/compareMe/compareMeCompleteController.java
@@ -0,0 +1,15 @@
+package compareMe;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+public class compareMeCompleteController {
+    public ImageView duplicateImage;
+    public ListView duplicates;
+    public Label currentNumDuplicate;
+
+    public void loadWindow() {
+
+    }
+}
