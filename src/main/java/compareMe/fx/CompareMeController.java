1abd008b62b6bd6ad141c7a98d64ca3fe41c98b2
Justin Guan <dengitzjg@gmail.com>
2017-01-06 16:00:51 -0500

    Added multithreading using JavaFX concurrency

--- src/main/java/compareMe/fx/CompareMeController.java
+++ src/main/java/compareMe/fx/CompareMeController.java
@@ -1,17 +1,20 @@
 package compareMe.fx;
 
 import compareMe.Comparator;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.nio.file.Files;
 
@@ -22,6 +25,7 @@ public class CompareMeController {
     public CheckBox secondCheck;
     public CheckBox autoDelete;
     public ToggleGroup hashFunction;
+    public VBox vbox;
 
     public void selectDirectory1(ActionEvent actionEvent) {
         directory.setText(this.selectDirectory());
@@ -34,47 +38,57 @@ public class CompareMeController {
     }
 
     public void beginCompare(ActionEvent actionEvent) {
-        try {
-            Comparator comparator = new Comparator();
-            RadioButton hash = (RadioButton) hashFunction.getSelectedToggle();
-            ArrayList<ArrayList<File>> duplicates = comparator.compare(directory.getText(), hash.getText(), recursive.isSelected());
-
-            if (autoDelete.isSelected() && duplicates.size() > 0) {
-                for (ArrayList<File> listOfFiles : duplicates) {
-                    boolean first = true;
-                    for (File f : listOfFiles) {
-                        if (first) {
-                            first = false;
-                            continue;
-                        }
-                        Files.delete(f.toPath()); // Use package java.nio.file to delete due to better error checking
-                    }
-                }
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Completed.fxml"));
-                Parent root = fxmlLoader.load();
-                Stage stage = new Stage();
-                stage.setTitle("Compare Me - Completed");
-                stage.setScene(new Scene(root, 300, 100));
-                stage.show();
-            } else if (duplicates.size() > 0) {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ManualDeleter.fxml"));
-                Parent root = fxmlLoader.load();
-                ManualDeleterController controller = fxmlLoader.<ManualDeleterController>getController();
-                controller.initWindow(duplicates);
-                Stage stage = new Stage();
-                stage.setTitle("Compare Me - Manual Deleter");
-                stage.setScene(new Scene(root));
-                stage.show();
-            } else {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Completed.fxml"));
-                Parent root = fxmlLoader.load();
-                Stage stage = new Stage();
-                stage.setTitle("Compare Me - Completed");
-                stage.setScene(new Scene(root, 300, 100));
-                stage.show();
+        Task<ArrayList<ArrayList<File>>> task = new Task<ArrayList<ArrayList<File>>>() {
+            @Override
+            protected ArrayList<ArrayList<File>> call() throws Exception {
+                vbox.setDisable(true);
+                Comparator comparator = new Comparator();
+                RadioButton hash = (RadioButton) hashFunction.getSelectedToggle();
+                return comparator.compare(directory.getText(), hash.getText(), recursive.isSelected());
             }
-        } catch (NoSuchAlgorithmException | IOException e) {
-            throw new RuntimeException("Something went wrong: " + e.toString());
-        }
+        };
+
+        new Thread(task).start();
+        task.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent t) {
+                vbox.setDisable(false);
+                try {
+                    ArrayList<ArrayList<File>> duplicates = task.getValue();
+                    if (autoDelete.isSelected() && duplicates.size() > 0) {
+                        for (ArrayList<File> listOfFiles : duplicates) {
+                            for (int i = 1; i < listOfFiles.size(); i++) {
+                                Files.delete(listOfFiles.get(i).toPath()); // Use package java.nio.file to delete due to better error checking
+                            }
+                        }
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Completed.fxml"));
+                        Parent root = fxmlLoader.load();
+                        Stage stage = new Stage();
+                        stage.setTitle("Compare Me - Completed");
+                        stage.setScene(new Scene(root, 300, 100));
+                        stage.show();
+                    } else if (duplicates.size() > 0) {
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ManualDeleter.fxml"));
+                        Parent root = fxmlLoader.load();
+                        ManualDeleterController controller = fxmlLoader.<ManualDeleterController>getController();
+                        controller.initWindow(duplicates);
+                        Stage stage = new Stage();
+                        stage.setTitle("Compare Me - Manual Deleter");
+                        stage.setScene(new Scene(root));
+                        stage.show();
+                    } else {
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Completed.fxml"));
+                        Parent root = fxmlLoader.load();
+                        Stage stage = new Stage();
+                        stage.setTitle("Compare Me - Completed");
+                        stage.setScene(new Scene(root, 300, 100));
+                        stage.show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 }
